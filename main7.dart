d0796b465849addfe08efd65145e03849cc4a7f7
talha siddiuque <talhasiddiquemeo007@gmail.com>
2024-02-11 01:33:56 +0500

    chcek

--- main7.dart
+++ main7.dart
@@ -1,12 +1,14 @@
-//Q7: Write a program to calculate root of any number.
-//i.e: √y = y½
+// Q7: Write a program to calculate root of any number.
+// i.e: √y = y½
 
+
+import 'dart:math';
 void main() {
-  num givennumber = 10;
-  num squareroot = sqrt(givennumber);
-  print(squareroot);
+  double y = 25; 
 
-  double y = 4;
-  double root = sqrt(y);
-  print("$y"(1 / 2));
+  double squareRoot = sqrt(y);
+
+  print('Square root of $y is $squareRoot');
 }
+
+
